40bff9a79de1b3c726e6b6ad787a3fbc6423720a
CHAEN <nuguziii@naver.com>
2020-12-21 18:43:21 +0900

    loss function->class

--- src/loss/DiceLoss.py
+++ src/loss/DiceLoss.py
@@ -5,40 +5,48 @@
 # Author:   Chae Eun Lee
 # Email:    nuguziii@cglab.snu.ac.kr
 #####################################################
+import torch.nn as nn
 
-def loss_softdice(target, output, weights=None):
-    '''
-    :param target: NxDxHxW LongTensor
-    :param output: NxCxDxHxW Variable
-    :param weights: C FloatTensor
-    :param ignore_index: int index to ignore from loss
-    :return:
-    '''
-    eps = 0.0001
-    encoded_target = output.detach() * 0
+class loss_softdice(nn.Module):
+    def __init__(self):
+        super(loss_softdice, self).__init__()
 
-    encoded_target.scatter_(1, target.unsqueeze(1), 1)
+    def forward(self, input, target, weights=None):
+        '''
+            :param target: NxDxHxW LongTensor
+            :param output: NxCxDxHxW Variable
+            :param weights: C FloatTensor
+            :param ignore_index: int index to ignore from loss
+            :return:
+            '''
+        eps = 0.0001
+        encoded_target = input.detach() * 0
 
-    if weights is None:
-        weights = 1
+        encoded_target.scatter_(1, target.unsqueeze(1), 1)
 
-    intersection = output * encoded_target
-    numerator = 2 * intersection.sum(2).sum(2).sum(2)
-    denominator = output + encoded_target
-    denominator = denominator.sum(2).sum(2).sum(2) + eps
-    loss_per_channel = weights * (1 - (numerator / denominator))
+        if weights is None:
+            weights = 1
 
-    return loss_per_channel.sum() / loss_per_channel.size(0)
+        intersection = input * encoded_target
+        numerator = 2 * intersection.sum(2).sum(2).sum(2)
+        denominator = input + encoded_target
+        denominator = denominator.sum(2).sum(2).sum(2) + eps
+        loss_per_channel = weights * (1 - (numerator / denominator))
 
+        return loss_per_channel.sum() / loss_per_channel.size(0)
 
-def loss_dice(target, output):
-    eps = 0.0001
-    _, output = output.max(1)
+class loss_dice(nn.Module):
+    def __init__(self):
+        super(loss_dice, self).__init__()
 
-    intersection = output * target
-    numerator = 2 * intersection.sum(1).sum(1).sum(1)
-    denominator = output + target
-    denominator = denominator.sum(1).sum(1).sum(1) + eps
-    loss_per_channel = 1 - (numerator / denominator)
+    def forward(self, input, target):
+        eps = 0.0001
+        _, output = input.max(1)
 
-    return loss_per_channel.sum() / loss_per_channel.size(0)
+        intersection = output * target
+        numerator = 2 * intersection.sum(1).sum(1).sum(1)
+        denominator = output + target
+        denominator = denominator.sum(1).sum(1).sum(1) + eps
+        loss_per_channel = 1 - (numerator / denominator)
+
+        return loss_per_channel.sum() / loss_per_channel.size(0)
